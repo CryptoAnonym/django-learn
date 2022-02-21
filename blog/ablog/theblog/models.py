6178e8f088ba5b2bd4415d22561cef2f68044bc0
aLitwin <newlitwin@gmail.com>
2022-02-21 20:03:58 +0100

    --add (add post with home website)

--- blog/ablog/theblog/models.py
+++ blog/ablog/theblog/models.py
@@ -1,5 +1,7 @@
+import django
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -9,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    
+    def get_absolute_url(self):
+        return reverse('article-detail', args=[str(self.id)])
