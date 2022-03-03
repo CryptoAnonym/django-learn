1352a0ed0254c41211432fdfae0d0aee37bd6652
aLitwin <newlitwin@gmail.com>
2022-03-03 11:33:20 +0100

    blog -add date

--- blog/ablog/theblog/models.py
+++ blog/ablog/theblog/models.py
@@ -2,12 +2,14 @@ import django
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_TAG = models.CharField(max_length=255, default="My BLOG! " )  # on nie zostawił
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
--- blog/ablog/theblog/views.py
+++ blog/ablog/theblog/views.py
@@ -11,6 +11,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post 
     template_name = 'home.html'
+    ordering = ['-post_date']   # odwraca numeracje po dacie
     #ordering = ['-id']   # odwraca numeracje 
 
 class ArticleDetailView(DetailView):
